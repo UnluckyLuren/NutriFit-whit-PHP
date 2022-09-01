a12812559bccb0cbb05e5e4998e7ac84aa2ff268
Luren <majorasliink@gmail.com>
2022-09-01 18:25:06 -0500

    Update html products

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//Imports
+// import  {crearHtml} from "./nutrifit_living/js/componentes";
+
+
+//constantes declaraciones
+
+
+
+
+// Eventos
+
+// Document.addEventListener("load", () => {
+
+
+
+// });
+
+
+
+
+
