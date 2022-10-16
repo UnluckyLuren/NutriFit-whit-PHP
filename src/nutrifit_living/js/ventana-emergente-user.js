13938014ee8bb18e857b73b2f4ab31df366c136a
Luren <majorasliink@gmail.com>
2022-10-16 14:03:04 -0500

    Update index

--- src/nutrifit_living/js/ventana-emergente-user.js
+++ src/nutrifit_living/js/ventana-emergente-user.js
@@ -1,26 +1,24 @@
-const open = document.getElementById( 'open'); 
+const open = document.getElementById('open'); 
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
+console.log(open)
+
 open.addEventListener('click', () => {
     modal_container.classList.add('show');
-
-    Body_scroll = document.getElementById('Body-scroll');
-    Body_scroll.classList.toggle('body-scroll');
-
+    console.log('si se añadio la clase');
 });
 
-// close.addEventListener('click', () => {
-//     modal_container.classList.remove('show');
-// });
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
 
 
 /* Mostrar menu emergente */
 
 // const Menu_emergente_Open = document.getElementById('Menu-emergente-image'),
-//     Menu_emergente = document.getElementById('Menu-emergente')
+//     Menu_emergente = document.getElementById('Menu-emergente'),
 //     Body_scroll = document.getElementById('Body-scroll')
-
 
 // Menu_emergente_Open.addEventListener('click', () => {
 
